644409cee1d2d514d345acdd11ab24a596171277
begi.dll <102294103+beeeegi@users.noreply.github.com>
2023-11-29 20:29:03 +0400

    updated main page

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const hamburger = document.querySelector('.hamburger')
 const hamburgerIcon = document.querySelector('.hamburger i')
 const dropdownMenu = document.querySelector('.dropdown')
+const navbar = document.getElementById("navbar");
+const sticky = navbar.getBoundingClientRect().top + window.scrollY;
 
+
+// hamburgeris funqcia
 hamburgerIcon.onclick = function() {
     dropdownMenu.classList.toggle('open');
 
@@ -16,3 +20,12 @@ hamburgerIcon.onclick = function() {
     }
 };
 
+
+// navigation function
+window.onscroll = function(){
+    if (window.scrollY >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
