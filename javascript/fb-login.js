64e83480a82c12724e23ebe7397352e9e14ce4a7
Begi <102294103+saur0nnn@users.noreply.github.com>
2023-12-02 02:57:49 +0400

    asd

--- javascript/fb-login.js
+++ javascript/fb-login.js
@@ -13,13 +13,43 @@ window.fbAsyncInit = function() {
         xfbml            : true,
         version          : 'v18.0'
     });
+};
+
+function startFacebookLogin() {
+    reloadFacebookSDK();
+
     FB.login(function(response) {
         if (response.authResponse) {
-            console.log('Welcome!  Fetching your information.... ');
-            FB.api('/me', {fields: 'name, email'}, function(response) {
-                document.getElementById("profile").innerHTML = "Good to see you, " + response.name + ". i see your email address is " + response.email
+            FB.api('/me', {fields: 'name'}, function(response) {
+                document.getElementById("profile").innerHTML = "Good to see you, " + response.name;
             });
         } else { 
-            console.log('User cancelled login or did not fully authorize.'); }
+            console.log('User cancelled login or did not fully authorize.');
+        }
     });
-};
+}
+
+function reloadFacebookSDK() {
+    var existingScript = document.getElementById('facebook-jssdk');
+    if (existingScript) {
+        existingScript.parentNode.removeChild(existingScript);
+    }
+
+    var js = document.createElement('script');
+    js.id = 'facebook-jssdk';
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    document.getElementsByTagName('head')[0].appendChild(js);
+}
+
+
+function startFacebookLogout() {
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            FB.logout(function(logoutResponse) {
+                document.getElementById("profile").innerHTML = "You are now logged out";
+            });
+        } else {
+            document.getElementById("profile").innerHTML = "you are not logged in";
+        }
+    });
+}
