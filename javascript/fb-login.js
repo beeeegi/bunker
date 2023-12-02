f84ed1e50b5a63cd8a61d9a41e0d729a4119bc8a
Begi <102294103+saur0nnn@users.noreply.github.com>
2023-12-02 16:47:32 +0400

    a

--- javascript/fb-login.js
+++ javascript/fb-login.js
@@ -10,8 +10,8 @@ function startFacebookLogin() {
     FB.login(function(response) {
         if (response.authResponse) {
             console.log('Welcome! Fetching your information....');
-            FB.api('/me', { fields: 'name, email' }, function(response) {
-                document.getElementById("profile").innerHTML = "Good to see you, " + response.name + ". I see your email address is " + response.email;
+            FB.api('/me', { fields: 'name' }, function(response) {
+                document.getElementById("profile").innerHTML = "Good to see you, " + response.name;
             });
         } else {
             console.log('User cancelled login or did not fully authorize.');
